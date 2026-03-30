25b4acd74d4aff618d240c4951c48cac6beb778f
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-30 19:42:56 +0700

    menyelesaikan perulangan while

--- Looping.cpp
+++ Looping.cpp
@@ -18,3 +18,7 @@ int main(){
     // Perulangan dengan WHILE
     cout << "PERULANGAN WHILE" << endl;
     x = 1 + rand() % 10;
+    while(x <= 5){
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    }
