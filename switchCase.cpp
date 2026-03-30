652fb527298a40fdfe18e12d9b74092913f440be
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-30 19:36:12 +0700

    prosedur

--- switchCase.cpp
+++ switchCase.cpp
@@ -36,3 +36,10 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+// prosedur
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
